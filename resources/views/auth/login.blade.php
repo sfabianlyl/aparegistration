5c31ff1662e03d264d363f170679f22438c7c1a6
sfabianlyl <sfabianlyl@gmail.com>
2023-06-06 12:31:33 +0800

    register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="w-auto h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
 
@@ -39,13 +39,12 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Register here') }}
                     </a>
-                @endif
+                
 
                 <x-button class="ml-3">
                     {{ __('Log in') }}
