687421f0b0d6a09f3157ae34a1a6c2aaee666fd2
sfabianlyl <sfabianlyl@gmail.com>
2023-06-27 17:39:19 +0800

    removeregister

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,9 +41,9 @@
             </div>
             <div class="flex items-center justify-end mt-4">
                 
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
                         {{ __('Register here') }}
-                    </a>
+                    </a> --}}
                 
 
                 <x-button class="ml-3">
