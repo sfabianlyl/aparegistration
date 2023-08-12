82292b73d86e476e5a1265d44a84290c99897c57
sfabianlyl <sfabianlyl@gmail.com>
2023-08-12 22:30:54 +0800

    attendance

--- resources/views/forms/parts/attendance.blade.php
+++ resources/views/forms/parts/attendance.blade.php
@@ -1,7 +1,7 @@
 <div class="row mb-5 align-items-center">
     <div class="col-4">Attending APA 2023?</div>
     <div class="col-8 attendance-container">
-        <label><input type="radio" name="{{$name}}" value="Yes" {{$value=="Yes"?"checked":""}}>Yes</label><br>
+        <label><input type="radio" name="{{$name}}" value="Yes" {{$value=="Yes"||$value==""?"checked":""}}>Yes</label><br>
         <label><input type="radio" name="{{$name}}" value="No" {{$value=="No"?"checked":""}}>No</label>
     </div>
 </div>
