2330541ecae580b0e7e1e2f2e9028e6299db82b9
sfabianlyl <sfabianlyl@gmail.com>
2023-09-08 14:43:21 +0800

    remove responsive action

--- resources/views/forms/parts/attendance.blade.php
+++ resources/views/forms/parts/attendance.blade.php
@@ -5,16 +5,3 @@
         <label><input type="radio" name="{{$name}}" value="No" {{$value=="No"?"checked":""}}>No</label>
     </div>
 </div>
-
-<script>
-    $(document).ready(function(){
-        $(".attendance-container input").on("change",function(){
-            if($(this).val()=="No"){
-                $("#form-self :input:not(.attendance-container input)").prop("readonly",true);
-            }else{
-                $("#form-self :input:not(.attendance-container input)").prop("readonly",false);
-            }
-        });
-        $(".attendance-container input").trigger("change");
-    });
-</script>
